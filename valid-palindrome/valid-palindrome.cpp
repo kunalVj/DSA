f3c319a27cd6af3cdbc94032933607b6750182b8
kunal vijay <106743611+kunalVj@users.noreply.github.com>
2023-05-28 00:50:11 +0530

    Time: 3 ms (91.37%) | Memory: 7.8 MB (29.16%) - LeetSync

---
+++ valid-palindrome/valid-palindrome.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+         string str;
+        for(int i=0;i<s.size();i++) {
+            if(isupper(s[i])) {
+                s[i] = tolower(s[i]);
+            }
+            if(isalnum(s[i])) {
+                str.push_back(s[i]);
+            }
+        }
+        string rev = str;
+        reverse(rev.begin(), rev.end());
+        return str == rev;
+    }
+};
