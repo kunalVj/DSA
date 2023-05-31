932de014d1cc6f2ca7cf47f001a15029c968d5d2
kunal vijay <106743611+kunalVj@users.noreply.github.com>
2023-05-31 23:40:41 +0530

    Time: 321 ms (60.00%) | Memory: 122.1 MB (54.66%) - LeetSync

--- time-needed-to-inform-all-employees/time-needed-to-inform-all-employees.cpp
+++ time-needed-to-inform-all-employees/time-needed-to-inform-all-employees.cpp
@@ -1,24 +1,23 @@
 class Solution {
 public:
-int maxTime = INT_MIN;
 
-void dfs(int currNode, int time,  vector<int> adj[], vector<int> &informTime){
+void dfs(int currNode, int time,  vector<vector<int>> &adj, vector<int> &informTime, int &maxTime){
     maxTime = max(maxTime, time); // max time taken to deliver the news to employee
 
     for(auto subordinate : adj[currNode]){  // iterate over adjacent nodes of current node with the time it took to get the news to the subordinate employee
-        dfs(subordinate, time + informTime[currNode], adj, informTime);
+        dfs(subordinate, time + informTime[currNode], adj, informTime, maxTime);
     }
 }
     int numOfMinutes(int n, int headID, vector<int>& manager, vector<int>& informTime) {
-            vector<int> adj[n];
+            vector<vector<int>> adj(n);
 
              for(int i = 0; i < n; i++){ // adjaceny list where ech index store the ids of subordinate employee
                  if(manager[i] != -1){   
                      adj[manager[i]].push_back(i);
                  }
              }
-
-             dfs(headID, 0, adj, informTime);
+             int maxTime = 0;
+             dfs(headID, 0, adj, informTime, maxTime);
 
              return maxTime;
     }
