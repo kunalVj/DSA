ad1b0e32750298cf99942376dce3f94bf1eaa117
kunal vijay <106743611+kunalVj@users.noreply.github.com>
2023-06-06 12:58:35 +0530

    Time: 4 ms (36.72%) | Memory: 6.7 MB (24.61%) - LeetSync

---
+++ minimum-genetic-mutation/minimum-genetic-mutation.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    int minMutation(string startGene, string endGene, vector<string>& bank) {
+        unordered_set<string> st (bank.begin(), bank.end());
+        queue<pair<string, int>> q;
+        q.push({startGene, 0});
+        st.erase(startGene);
+
+        while(!q.empty()){
+            string gene = q.front().first;
+            int count = q.front().second;
+            q.pop();
+
+            if(gene == endGene){
+                return count;
+            }
+
+            for(char c: "ACGT"){
+                for(int i = 0; i < gene.size(); i++){
+                    char original = gene[i];
+                    gene[i] = c;
+                    if(st.find(gene) != st.end()){
+                        q.push({gene, count + 1});
+                        st.erase(gene);
+                    }
+
+                    gene[i] = original;
+                }
+                
+            }
+
+        }
+
+        return -1;
+
+    }
+};
