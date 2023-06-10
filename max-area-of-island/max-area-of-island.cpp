53be958f44b36b433b112ca61644945fc5ff3769
kunal vijay <106743611+kunalVj@users.noreply.github.com>
2023-06-10 13:52:01 +0530

    Time: 20 ms (69.95%) | Memory: 23.9 MB (46.55%) - LeetSync

--- max-area-of-island/max-area-of-island.cpp
+++ max-area-of-island/max-area-of-island.cpp
@@ -6,35 +6,36 @@ public:
 
         queue<pair<int,int>> q;
 
-        int area = 0;
+        int area = 0;    // if we encounter different group of islands we will keep updating the maximum area of encountered islands
 
         int dx[] = {-1, 0, 1, 0};
         int dy[] = {0, 1, 0, -1};
-         for(int i = 0; i < n; i++){
+
+         for(int i = 0; i < n; i++){      // traverse the grid
              for(int j = 0 ; j < m; j++){
-                 if(grid[i][j] == 1){
-                     int ans = 1;
-                     grid[i][j] = 0;
-                     q.push({i,j});
+                 if(grid[i][j] == 1){  
+                     int ans = 1;        // if we encounter the island start storing it's area in ans, area of 1 island = 1
+                     grid[i][j] = 0;    // mark the island as visited by turning 0 so we can't visit it again
+                     q.push({i,j});       // push the node to traverse it's adjacent node
 
                      while(!q.empty()){
                      int x = q.front().first;
                      int y = q.front().second;
                      q.pop();
 
-                     for(int k = 0; k < 4; k++){
+                     for(int k = 0; k < 4; k++){    // moving in all 4 direction 
                          int nrow = x + dx[k];
                          int ncol = y + dy[k];
 
-                         if(nrow >= 0 && ncol >= 0 && nrow < n && ncol < m && grid[nrow][ncol] == 1){
-                             ans++;
-                             grid[nrow][ncol] = 0;
-                             q.push({nrow,ncol});
+                         if(nrow >= 0 && ncol >= 0 && nrow < n && ncol < m && grid[nrow][ncol] == 1){ // if node is valid
+                             ans++;             // if we find island adjacent to previous we increase the area count
+                             grid[nrow][ncol] = 0; // mark it visited
+                             q.push({nrow,ncol});   
                          }
                      }
                   }
 
-                  area = max(area, ans);
+                  area = max(area, ans);   // if the new area is greater than previous one than update otherwise it will remain same
 
               }
           }
