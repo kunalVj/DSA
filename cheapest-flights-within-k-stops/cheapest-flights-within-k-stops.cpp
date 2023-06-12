cada532ff232052c56e100f7aa851ce287b41186
kunal vijay <106743611+kunalVj@users.noreply.github.com>
2023-06-12 15:16:08 +0530

    Time: 25 ms (70.00%) | Memory: 13.6 MB (45.17%) - LeetSync

--- cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.cpp
+++ cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.cpp
@@ -1,18 +1,20 @@
 class Solution {
 public:
+
+// use the dijkstra algo with some changes :- don't use priority queue and use queue, store the stops first in queue then src and then dist i.e cost
     int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
 
         vector<pair<int,int>>adj[n];
         
-        for(auto it : flights){
+        for(auto it : flights){ // create the graph
             adj[it[0]].push_back({it[1], it[2]});
         }
 
         queue<pair<int,pair<int,int>>> q;
-        q.push({0,{src, 0}});
+        q.push({0,{src, 0}});     // {stop, {src, cost}}     
 
         vector<int> dist(n, 1e9);
-        dist[src] = 0;
+        dist[src] = 0;       // src distance will be 0
 
         while(!q.empty()){
             auto i = q.front();
@@ -21,24 +23,24 @@ public:
             int node = i.second.first;
             int cost = i.second.second;
 
-            if(stops > k) continue;
+            if(stops > k) break;       // if stops exceeds k no need to process further 
 
-            for(auto it : adj[node]){
+            for(auto it : adj[node]){    // process the nodes neighbour
                 int adjNode = it.first;
                 int edgeW = it.second;
 
-                if(cost + edgeW < dist[adjNode] && stops <= k){
+                if(cost + edgeW < dist[adjNode] && stops <= k){      // if the new cost is less than existing then update the new one while k <= stops
                     dist[adjNode] = cost + edgeW;
-                    q.push({stops + 1, {adjNode, cost + edgeW}});
+                    q.push({stops + 1, {adjNode, cost + edgeW}});   
                 }
             }
         }
 
-        if(dist[dst] == 1e9){
+        if(dist[dst] == 1e9){     // if we don't reach destination then return -1
             return -1;
         }
 
-        return dist[dst];
+        return dist[dst];     // if destination is reached then return the cost
 
     }
 };
