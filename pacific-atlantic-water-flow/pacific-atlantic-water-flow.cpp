0f139b21a70cabd04578d2f7aaae26e9b40590d9
kunal vijay <106743611+kunalVj@users.noreply.github.com>
2023-06-14 23:26:27 +0530

    Time: 55 ms (36.05%) | Memory: 19.5 MB (36.06%) - LeetSync

--- pacific-atlantic-water-flow/pacific-atlantic-water-flow.cpp
+++ pacific-atlantic-water-flow/pacific-atlantic-water-flow.cpp
@@ -1,5 +1,12 @@
 class Solution {
 public:
+// intutuion = we se if water can go from ocean to land, and if current coordinate is greater than it's neighbouring
+// coordinates then water can not flow to them
+// only water can flow to those neighbours who are greater than current coordinate
+// take 2 grids for both pacific and atlantic ocean and check in which island water flow and mark them visited and push into queue
+// after doing for both oceans now store the answer in ans vector, if a same coordinate is marked visited
+// in both pacific and atlantic grid then it means from that coordinate water can flow to both oceans
+
 void bfs(vector<vector<int>>& heights, queue<pair<int,int>> &q, vector<vector<int>> &vis){
     int n = heights.size();
     int m = heights[0].size();
@@ -16,6 +23,9 @@ void bfs(vector<vector<int>>& heights, queue<pair<int,int>> &q, vector<vector<in
             int nrow = x + dx[k];
             int ncol = y + dy[k];
 
+//along with all the eligibility criteria the next coordinates height must be greater than or equal to 
+// the current coordinates heights cause we are moving from ocean to island and
+//we want the water to go from island to ocean.
             if(nrow >= 0 && ncol >= 0 && nrow < n && ncol < m && heights[nrow][ncol] >= heights[x][y] && 
             !vis[nrow][ncol]){
                 q.push({nrow, ncol});
@@ -34,6 +44,7 @@ void bfs(vector<vector<int>>& heights, queue<pair<int,int>> &q, vector<vector<in
         queue<pair<int, int>> pac;
         queue<pair<int,int>> atl;
 
+//push the respective boundary elements to the pacific and atlantic queue and visited array
         for(int i = 0; i < n; i++){
             for(int j = 0; j < m; j++){
                 if(i == 0 || j == 0){
@@ -48,7 +59,7 @@ void bfs(vector<vector<int>>& heights, queue<pair<int,int>> &q, vector<vector<in
             }
         }
 
-        vector<vector<int>> ans;
+        vector<vector<int>> ans; // to store final answer
 
         bfs(heights, pac, pacific);
         bfs(heights, atl, atlantic);
