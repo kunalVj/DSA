1190776c426434ce03ac0bfa28157bdaef0b6075
kunal vijay <106743611+kunalVj@users.noreply.github.com>
2023-06-25 13:18:52 +0530

    Time: 94 ms (32.76%) | Memory: 10.8 MB (69.62%) - LeetSync

--- longest-common-subsequence/longest-common-subsequence.cpp
+++ longest-common-subsequence/longest-common-subsequence.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-int longestCommonSubsequence(string &text1, string &text2,int m,int n,int memo[][1001]) {
+int longestCommonSubsequence(string &text1, string &text2,int m,int n,int memo[1001][1001]) {
         if(m==0 || n==0){
             return 0;
         }
