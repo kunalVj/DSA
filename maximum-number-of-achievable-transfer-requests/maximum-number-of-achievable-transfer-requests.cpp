c7d4074a12a4e11c6f923d5c05254843c02b5b9e
kunal vijay <106743611+kunalVj@users.noreply.github.com>
2023-07-02 14:21:08 +0530

    Time: 112 ms (63.73%) | Memory: 8.7 MB (92.16%) - LeetSync

--- maximum-number-of-achievable-transfer-requests/maximum-number-of-achievable-transfer-requests.cpp
+++ maximum-number-of-achievable-transfer-requests/maximum-number-of-achievable-transfer-requests.cpp
@@ -2,6 +2,7 @@ class Solution {
 public:
 void solve(vector<vector<int>> &requests, vector<int> &indegree, int n, int &ans, int index, int count){
     if(index == requests.size()){
+        // Check if all buildings have an in-degree of 0.
         for(int i = 0; i < n; i++){
             if(indegree[i] != 0){
                 return;
@@ -12,13 +13,20 @@ void solve(vector<vector<int>> &requests, vector<int> &indegree, int n, int &ans
         return;
     }
 
+// Consider this request, increment and decrement for the buildings involved.
     indegree[requests[index][0]]--;
     indegree[requests[index][1]]++;
 
+// Move on to the next request and also increment the count of requests.
+
     solve(requests, indegree, n, ans, index + 1, count + 1);
+
+// Backtrack to the previous values to move back to the original state before the second recursion.
 
     indegree[requests[index][0]]++;
     indegree[requests[index][1]]--;
+
+ // Ignore this request and move on to the next request without incrementing the count.
 
     solve(requests, indegree, n, ans, index + 1, count);
 }
@@ -32,3 +40,6 @@ void solve(vector<vector<int>> &requests, vector<int> &indegree, int n, int &ans
         return ans;
     }
 };
+
+// tc = O(2^n * m) , n = np. of req, m = no.of buildings
+// sc = O(n + m)
