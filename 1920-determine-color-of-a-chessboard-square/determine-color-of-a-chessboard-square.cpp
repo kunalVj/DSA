86ee80c5554a4961b4c52fd3b621432e46c84a4a
kunal vijay <106743611+kunalVj@users.noreply.github.com>
2023-07-19 22:02:37 +0530

    Time: 0 ms (100.00%) | Memory: 5.8 MB (75.75%) - LeetSync

--- 1920-determine-color-of-a-chessboard-square/determine-color-of-a-chessboard-square.cpp
+++ 1920-determine-color-of-a-chessboard-square/determine-color-of-a-chessboard-square.cpp
@@ -3,17 +3,15 @@ public:
     bool squareIsWhite(string coordinates) {
         char letter = coordinates[0], num = coordinates[1];
 
-        if(letter == 'a' || letter == 'c' || letter == 'e' || letter == 'g'){
-            if(num == '1' || num == '3' || num == '5' || num == '7'){
+        if((letter & 1 && num & 1) || (!(letter & 1) && !(num & 1))){ // if odd
                 return false;
-            }
         }
 
-        else if(letter == 'b' || letter == 'd' || letter == 'f' || letter == 'h'){
-            if(num == '2' || num == '4' || num == '6' || num == '8'){
-                return false;
-            }
-        }
+        // else if(letter == 'b' || letter == 'd' || letter == 'f' || letter == 'h'){
+        //     if(num == '2' || num == '4' || num == '6' || num == '8'){
+        //         return false;
+        //     }
+        // }
 
         return true;
     }
