eb5f2e9938bf82edafae664cef05258fe3f17e20
kunal vijay <106743611+kunalVj@users.noreply.github.com>
2023-07-23 02:52:50 +0530

    Time: 9 ms (75.00%) | Memory: 22.4 MB (37.50%) - LeetSync

---
+++ 2892-check-if-array-is-good/check-if-array-is-good.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool isGood(vector<int>& nums) {
+        int n = nums.size();
+        n--;
+        sort(nums.begin(), nums.end());
+        for(int i=0;i<n;i++){
+            if(nums[i]!=i+1){
+                return false;
+            }
+        }
+        if(nums[n]!=n){
+            return false;
+        }
+        return true;
+        
+    }
+};
