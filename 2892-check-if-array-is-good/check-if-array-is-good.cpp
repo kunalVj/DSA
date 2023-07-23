5e712683ec54d001affd35474ab7667328c8c664
kunal vijay <106743611+kunalVj@users.noreply.github.com>
2023-07-23 13:15:00 +0530

    Time: 8 ms (100.00%) | Memory: 22.9 MB (12.50%) - LeetSync

--- 2892-check-if-array-is-good/check-if-array-is-good.cpp
+++ 2892-check-if-array-is-good/check-if-array-is-good.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     bool isGood(vector<int>& nums) {
-        int n = nums.size();
+    /*    int n = nums.size();
         n--;
         sort(nums.begin(), nums.end());
         for(int i=0;i<n;i++){
@@ -12,7 +12,28 @@ public:
         if(nums[n]!=n){
             return false;
         }
+        return true; */
+
+        int n = *max_element(nums.begin(), nums.end());
+        int maxi = 0;
+        vector<int> freq(n+1, 0);
+        if(nums.size() < n+1) return false;
+        for(auto i : nums){
+            freq[i]++;
+          
+        }
+
+        for(int i = 0; i <= n; i++){
+            if(freq[i] == 0 && freq[i] > 2) return false;
+            if(freq[i] == 2 && i != n) return false;
+            if(freq[n] != 2){
+                return false;
+            }
+        }
+
         return true;
+
+
         
     }
 };
