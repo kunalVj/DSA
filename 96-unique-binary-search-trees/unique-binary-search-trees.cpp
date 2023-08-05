d892b0b1fa51e99201f4fb9e39b390e004b2c200
kunal vijay <106743611+kunalVj@users.noreply.github.com>
2023-08-05 14:54:19 +0530

    Time: 0 ms (100.00%) | Memory: 6 MB (73.08%) - LeetSync

--- 96-unique-binary-search-trees/unique-binary-search-trees.cpp
+++ 96-unique-binary-search-trees/unique-binary-search-trees.cpp
@@ -1,7 +1,29 @@
 class Solution {
 public:
+int solve(int n, vector<int> &dp){
+    if(n <= 1){
+        return 1;
+    }
+
+    if(!dp[n]){
+        for(int i = 1; i <= n; i++){
+            dp[n] += solve(i-1, dp) * solve(n-i, dp);
+        }
+    }
+
+    return dp[n];
+}
     int numTrees(int n) {
-        if(n <= 1){
+
+      vector<int> dp (n+1, 0);
+
+      return solve(n, dp);
+
+    }
+};
+
+/* recurssion
+ if(n <= 1){
             return 1;
         }
         int result = 0;
@@ -11,5 +33,4 @@ public:
         }
 
         return result;
-    }
-};
+*/
