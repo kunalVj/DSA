384002adf7ad1595305a837d77e3a87a648be3ee
kunal vijay <106743611+kunalVj@users.noreply.github.com>
2023-08-10 12:03:46 +0530

    Time: 3 ms (91.34%) | Memory: 14 MB (50.05%) - LeetSync

--- 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.cpp
+++ 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.cpp
@@ -5,8 +5,6 @@ public:
     
     bool search(vector<int>& nums, int target) {
 
-        sort(nums.begin(), nums.end());
-
         for(int i = 0; i < nums.size(); i++){
             if(nums[i] == target){
                 return true;
